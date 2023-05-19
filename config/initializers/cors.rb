481d7c51e7cf08d46ef9870a3de31a08a091dc9b
yetemegn-telaye <lemma19yet@gmail.com>
2023-05-19 13:12:07 +0300

    Update readme

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3001"
-
-    resource "*",
+    origins 'localhost:3001'  # Replace with your React app's URL
+      
+    resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
   end
 end
